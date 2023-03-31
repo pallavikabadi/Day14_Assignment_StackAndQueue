216f21b9415126ca8d5dfd796886370e83ce48a9
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-31 14:13:48 +0530

    Added:Peek And Pop Operation In Stack

--- src/com/bridgelabz/StackMain.java
+++ src/com/bridgelabz/StackMain.java
@@ -2,10 +2,29 @@ package com.bridgelabz;
 
 public class StackMain {
     public static void main(String[] args) {
-        Stacks <Integer> stacks = new Stacks<>();
+    //UC1:
+        System.out.println("Stack :-");
+    Stacks<Integer> stacks = new Stacks<>();
         stacks.push(70);
         stacks.push(30);
         stacks.push(56);
-        stacks.print();
+        stacks.display();
+        System.out.println();
+    //UC2:
+        System.out.println( "Peak Element");
+        stacks.peak();
+        System.out.println();
+        System.out.println( "Popped Element");
+    Integer poppedData;
+       do {
+        poppedData = stacks.pop();
+        if (poppedData == null)
+            System.out.println("Stack is Empty");
+        else
+            System.out.println("The Element popped is " + poppedData);
+        stacks.display();
     }
+       while (poppedData != null);
+        System.out.println("-----");
+}
 }
--- src/com/bridgelabz/Stacks.java
+++ src/com/bridgelabz/Stacks.java
@@ -6,11 +6,18 @@ public class Stacks<T>{
     LinkedList <T> linkedList = new LinkedList<>();
 
     public void push(T data) {
-
         linkedList.push(data);
     }
     public void display() {
+        // linkedList.display();
+    }
+    public T pop() {
+        return linkedList.pop();
+    }
+    public void peak() {
 
-       // linkedList.display();
+        Integer peakedData = (Integer) linkedList.pop();
+        System.out.println("The Peak Element = "+peakedData);
+        linkedList.push((T) peakedData);
     }
 }
