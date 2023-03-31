76dba363b52b0564df089b0beb174da4da10a5e8
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-03-31 14:00:30 +0530

    Added:Stack And Queue

---
+++ src/com/bridgelabz/StackMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class StackMain {
+    public static void main(String[] args) {
+        Stacks <Integer> stacks = new Stacks<>();
+        stacks.push(70);
+        stacks.push(30);
+        stacks.push(56);
+        stacks.print();
+    }
+}
---
+++ src/com/bridgelabz/Stacks.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.LinkedList;
+
+public class Stacks<T>{
+    LinkedList <T> linkedList = new LinkedList<>();
+
+    public void push(T data) {
+
+        linkedList.push(data);
+    }
+    public void display() {
+
+       // linkedList.display();
+    }
+}
